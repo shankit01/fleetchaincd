c3dd021848c48fcab9b71e501415cbd481bc9272
mamandal <mamandal@in.ibm.com>
2017-04-08 22:42:03 +0530

    changed part1.js

--- public/js/part1.js
+++ public/js/part1.js
@@ -84,9 +84,9 @@ $(document).on('ready', function() {
 //			$('.createball').css('border', '2px dashed #fff');						//reset
 //		}
 			
-		var	driverdetailsmail=$('input[name="checkdriveremail"]').val().replace(' ', '');
-		
-		$('input[name="driverdetailsemail"]').val(driverdetailsmail);
+//		var	driverdetailsmail=$('input[name="checkdriveremail"]').val().replace(' ', '');
+//		
+//		$('input[name="driverdetailsemail"]').val(driverdetailsmail);
 		showDriverDetailsPanel();
 		
 		return false;
