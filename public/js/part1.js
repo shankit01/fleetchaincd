6934a3bee503ee53a84030af526fb19aa4e491a4
mamandal <mamandal@in.ibm.com>
2017-04-15 15:56:41 +0530

    added driver list

--- public/js/part1.js
+++ public/js/part1.js
@@ -115,20 +115,20 @@ $(document).on('ready', function() {
 	});
 	
 	
-//	$('#driverlistLink').click(function(){
-//		
-//		console.log('Retrieve list of drivers');
-//		
-//		var driverlistobj = 	{
-//				type: 'listdriver',
-//				v: 1
-//			};
-//		
-//		console.log('listing drivers, sending', driverlistobj);
-//		ws.send(JSON.stringify(driverlistobj));
-//		
-//		//showDriverListPanel();
-//	});
+	$('#driverlistLink').click(function(){
+		
+		console.log('Retrieve list of drivers');
+		
+		var driverlistobj = 	{
+				type: 'listdriver',
+				v: 1
+			};
+		
+		console.log('listing drivers, sending', driverlistobj);
+		ws.send(JSON.stringify(driverlistobj));
+		
+		showDriverListPanel();
+	});
 
 	
 	//marble color picker
@@ -351,10 +351,10 @@ function connect_to_server(){
 				console.log('rec', msgObj.msg, msgObj);
 				build_ball(msgObj.marble);
 			}
-//			if(msgObj.eachdriver){
-//				console.log('rec', msgObj.msg, msgObj);
-//				build_driver(msgObj.eachdriver);
-//			}
+			if(msgObj.eachdriver){
+				console.log('rec', msgObj.msg, msgObj);
+				build_driver(msgObj.eachdriver);
+			}
 			if(msgObj.msg === 'driver'){
 				console.log('rec', msgObj.msg, msgObj.driver);
 
@@ -426,23 +426,23 @@ function build_ball(data){
 	return html;
 }
 
-//function build_driver(data){
-//	var html = '';
-//	var colorClass = '';
-//	var size = '12';
-//	
-//	data.firstname = escapeHtml(data.firstname);
-//	data.lastname = escapeHtml(data.lastname);
-//	data.email = escapeHtml(data.email);
-//	data.password = escapeHtml(data.password);
-//	
-//	console.log('got a driver: ', data.email);
-//	if(!$('#' + data.email).length){								//only populate if it doesn't exists
-//		//if(data.size == 16) size = '12';
-//		//if(data.color) colorClass = data.color.toLowerCase();
-//		
-//		html += '<span id="' + data.email + '" style="color:#fff' + ' First Name="' + data.firstname + ' Last Name="' + data.lastname+ '" Passwordr="' + data.password + '"></span>';
-//		$('#drlistwrap').append(html);
-//	}
-//	return html;
-//}
+function build_driver(data){
+	var html = '';
+	var colorClass = '';
+	var size = '12';
+	
+	data.firstname = escapeHtml(data.firstname);
+	data.lastname = escapeHtml(data.lastname);
+	data.email = escapeHtml(data.email);
+	data.password = escapeHtml(data.password);
+	
+	console.log('got a driver: ', data.email);
+	if(!$('#' + data.email).length){								//only populate if it doesn't exists
+		//if(data.size == 16) size = '12';
+		//if(data.color) colorClass = data.color.toLowerCase();
+		
+		html += '<span id="' + data.email + '" style="color:#fff' + ' First Name="' + data.firstname + ' Last Name="' + data.lastname+ '" Passwordr="' + data.password + '"></span>';
+		$('#drlistwrap').append(html);
+	}
+	return html;
+}
