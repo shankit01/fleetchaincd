63c24ab6e6949b1566661c78ea3175a1a1bdf123
mamandal <mamandal@in.ibm.com>
2017-04-16 00:04:32 +0530

    changed

--- public/js/part1.js
+++ public/js/part1.js
@@ -365,6 +365,7 @@ function connect_to_server(){
 //				$('input[name="driverdetailspassword"]').val(msgObj.eachdriver.password);
 //				showDriverDetailsPanel();
 				$('#driverdetailslist').append(msgObj.eachdriver.email);
+				showDriverListPanel();
 				
 			}
 //			if(msgObj.msg === 'driverlistcompleted'){
