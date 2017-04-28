0b7af8e070423e15cc8eae1410a2bdb384b977d9
mamandal <mamandal@in.ibm.com>
2017-04-28 15:57:26 +0530

    changed app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
  * Copyright (c) 2015 IBM Corp.
  *
  * All rights reserved. 
- *
  * Contributors:
  *   David Huffman - Initial implementation
  *******************************************************************************/
@@ -255,7 +254,7 @@ ibc.load(options, function (err, cc){														//parse/load chaincode, respo
 		// ---- To Deploy or Not to Deploy ---- //
 		if(!cc.details.deployed_name || cc.details.deployed_name === ''){
 			console.log('Chaincode deployment issue');
-			cc.deploy('init', ['99','nagware'], {delay_ms: 30000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
+			cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
 				check_if_deployed(e, 1);
 			});
 		}
