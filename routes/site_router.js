cc5146b9a8e5d017e40a2374cf0d34413a4ae690
mamandal <mamandal@in.ibm.com>
2017-05-01 21:30:21 +0530

    changed...

--- routes/site_router.js
+++ routes/site_router.js
@@ -9,18 +9,19 @@
  *   David Huffman - Initial implementation
  *******************************************************************************/
 var express = require('express');
-//var session = require('express-session');
+var session = require('express-session');
 var router = express.Router();
 var setup = require('../setup.js');
 //var ssn;
 
 //anything in here gets passed to JADE template engine
-function build_bag(){
+function build_bag(request){
 	return {
 				setup: setup,								//static vars for configuration settings
 				e: process.error,							//send any setup errors
 				jshash: process.env.cachebust_js,			//js cache busting hash (not important)
 				csshash: process.env.cachebust_css,			//css cache busting hash (not important)
+				ssn:request.session,
 			};
 }
 
@@ -33,27 +34,28 @@ router.route('/').get(function(req, res){
 });
 
 router.route('/login').get(function(req, res){
-	res.render('login.jade', {title: 'Please Sign In To Fleetchain'});
+	//ssn=req.session;
+	res.render('login.jade', {title: 'Please Sign In To Fleetchain',bag: build_bag(req)});
 });
 
 // ============================================================================================================================
 // Part 1
 // ============================================================================================================================
 router.route('/p1').get(function(req, res){
-	res.render('part1', {title: 'Marbles Part 1', bag: build_bag()});
+	res.render('part1', {title: 'Fleetchain Uberdotcom', bag: build_bag(req)});
 });
 router.route('/p1/:page?').get(function(req, res){
-	res.render('part1', {title: 'Marbles Part 1', bag: build_bag()});
+	res.render('part1', {title: 'Fleetchain Uberdotcom', bag: build_bag(req)});
 });
 
 // ============================================================================================================================
 // Part 2
 // ============================================================================================================================
 router.route('/p2').get(function(req, res){
-	res.render('part2', {title: 'Marbles Part 2', bag: build_bag()});
+	res.render('part2', {title: 'Fleetchain Uberdotcom', bag: build_bag(req)});
 });
 router.route('/p2/:page?').get(function(req, res){
-	res.render('part2', {title: 'Marbles Part 2', bag: build_bag()});
+	res.render('part2', {title: 'Fleetchain Uberdotcom', bag: build_bag(req)});
 });
 
 module.exports = router;
